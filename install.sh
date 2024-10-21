150cfc6955221dfe88afc33667bbe3e5fcf7a3b9
sin1kbk <72619395+sin1kbk@users.noreply.github.com>
2024-10-21 17:27:43 +0900

    feat: chagne VAGRANT_LOG info to warn

--- install.sh
+++ install.sh
@@ -327,7 +327,7 @@ After=network.target
 
 [Service]
 Environment='VAGRANT_HOME=${storage_mount_path}/.vagrant.d'
-Environment='VAGRANT_LOG=info'
+Environment='VAGRANT_LOG=warn'
 Type=simple
 User=${OS_USER_NAME}
 Group=${OS_USER_NAME}
